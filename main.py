24e85d91a953bb9584a80a59e52c4e5f48af1e16
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2023-05-06 19:01:31 +0200

    Minimal working game

---
+++ main.py
@@ -0,0 +1,73 @@
+import time
+
+import pygame
+
+import player
+import settings
+
+
+pygame.init()
+screen = pygame.display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+running = True
+
+snakes = [
+    player.Snake(
+        "Janne",
+        "red",
+        pygame.K_n,
+        pygame.K_m,
+        screen,
+        clock,
+    ),
+    player.Snake(
+        "Henner",
+        "orange",
+        pygame.K_a,
+        pygame.K_s,
+        screen,
+        clock,
+    ),
+]
+lines = []
+
+paused = False
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(settings.SCREEN_BACKGROUND_COLOR)
+
+    for line in lines:
+        pygame.draw.line(*line)
+
+    keys = pygame.key.get_pressed()
+
+    deaths = sum(snake.death for snake in snakes)
+    if deaths >= len(snakes) - 1:
+        if not paused:
+            for snake in snakes:
+                if not snake.death:
+                    msg = f"{snake.name} gewinnt!"
+                    print(msg)
+        paused = True
+
+        if keys[pygame.K_SPACE]:
+            paused = False
+            lines = []
+            for snake in snakes:
+                snake.restart()
+        else:
+            continue
+
+    for snake in snakes:
+        if snake.death:
+            continue
+        snake.drive(keys, lines)
+
+    pygame.display.flip()
+
+time.sleep(2)
+pygame.quit()
---
+++ player.py
@@ -0,0 +1,82 @@
+import math
+import random
+
+import settings
+
+SPEED_PER_FRAME = 5
+ANGLE_DELTA = 10
+LINE_WIDTH = 5
+
+HOLE_FREQUENCY_RANGE = (100, 200)
+HOLE_WIDTH_RANGE = (2, 6)
+
+
+class SnakeDeath(Exception):
+    """Thrown if snake dies"""
+
+
+class Snake:
+    def __init__(self, name, color, left_key, right_key, screen, clock):
+        self.name = name
+        self.x = random.randint(settings.START_MARGIN, settings.SCREEN_WIDTH - settings.START_MARGIN)
+        self.y = random.randint(settings.START_MARGIN, settings.SCREEN_HEIGHT - settings.START_MARGIN)
+        self.color = color
+        self.angle = random.randint(0, 360)
+        self.left_key = left_key
+        self.right_key = right_key
+        self.screen = screen
+        self.clock = clock
+        self.death = False
+        self.hole = random.randint(*HOLE_FREQUENCY_RANGE)
+        self.hole_width = random.randint(*HOLE_WIDTH_RANGE)
+
+    def drive(self, keys, lines):
+        if keys[self.left_key]:
+            self.angle += ANGLE_DELTA
+        if keys[self.right_key]:
+            self.angle -= ANGLE_DELTA
+
+        speed = self.clock.tick(60) / SPEED_PER_FRAME
+        nx = self.x + speed * math.cos(math.radians(-self.angle))
+        ny = self.y + speed * math.sin(math.radians(-self.angle))
+
+        self.check(nx, ny)
+
+        if self.hole >= 0:
+            lines.append((self.screen, self.color, (self.x, self.y), (nx, ny), LINE_WIDTH))
+
+        if self.hole == -self.hole_width:
+            self.hole = random.randint(*HOLE_FREQUENCY_RANGE)
+            self.hole_width = random.randint(*HOLE_WIDTH_RANGE)
+
+        self.x = nx
+        self.y = ny
+        self.hole -= 1
+
+    def check(self, nx, ny):
+        if nx < 0:
+            self.death = True
+            return
+        if nx > settings.SCREEN_WIDTH:
+            self.death = True
+            return
+        if ny < 0:
+            self.death = True
+            return
+        if ny > settings.SCREEN_HEIGHT:
+            self.death = True
+            return
+
+        try:
+            back_color = self.screen.get_at((round(nx), round(ny)))
+        except IndexError:
+            self.death = True
+            return
+        if back_color != settings.SCREEN_BACKGROUND_COLOR:
+            self.death = True
+            return
+
+    def restart(self):
+        self.death = False
+        self.x = random.randint(settings.START_MARGIN, settings.SCREEN_WIDTH - settings.START_MARGIN)
+        self.y = random.randint(settings.START_MARGIN, settings.SCREEN_HEIGHT - settings.START_MARGIN)
